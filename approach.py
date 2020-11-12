123423c1fa07d808421cb57599ab5ef0c84d77e7
Ariana Gonçalves <arygoncalves15@gmail.com>
2020-11-12 03:48:01 +0000

    saving changes

--- approach.py
+++ approach.py
@@ -52,10 +52,14 @@ class DeadLock(Approach):
                 self.__state = self.unstuck_state
             else:
                 self.search_solution = False
-                self.is_possible = False # deve ser true
+                self.is_possible = True
 
         elif self.__state == self.unstuck_state:
-            self.__state = self.search_state
+            if status == True:
+                self.__state = self.search_state
+            else:
+                self.search_solution = False
+                self.is_possible = False
                 
         #elif state is traceback_state:
         #    if traceback == None:
@@ -78,20 +82,20 @@ class DeadLock(Approach):
                 print(x, y)
 
             path_keeper = None
-            for i in range(0, len(path)-2):
-                if i == 0:
+            for j in range(0, len(path)-2):
+                if j == 0:
                     x,y = self.astate['keeper']
                     keeper = self.gridmap[x][y]
                 else: 
-                    keeper = path[i-1]
+                    keeper = path[j-1]
                 
-                obj_box = path[i+1]
-                box = path[i]
+                obj_box = path[j+1]
+                box = path[j]
                 node = oposite(self.gridmap, box, obj_box)
                 path_keeper = search(self.gridmap, keeper, node, 'keeper')
 
                 if path_keeper:
-                    self.refresh(box, obj_box, keeper, node)
+                    self.refresh_gridmap(box, obj_box, keeper, node)
                     box = obj_box
                     print("PATH KEEPER")
                     for node in path_keeper:
@@ -103,27 +107,71 @@ class DeadLock(Approach):
                     print(keeper.position)
                     print("Box")
                     print(box.position)
-                    break
+                    
+                    # obtem o caminho passando pela caixa existente
+                    box = self.box_priority[1] if i == 0 else self.box_priority[0]
+                    tmp = box.symbol 
+                    box.symbol = '-'
+                    self.desired_kpath = search(self.gridmap, keeper, node, 'keeper')
+                    box_symbol = tmp
+
+                    return False
         
         return True
 
     #def traceback_state():
 
     def unstuck_state(self):
-        pass
+        self.reset_gridmap()
+        print("IM STUCK STEPBRO")
+        self.box_priority.reverse()
+        self.goal_priority.reverse()
+        
+        box = self.box_priority[0]
 
-    def refresh(self, box, obj_box, keeper, obj_keeper):
+        print("Nodes")   
+        for n in children_boxes(box, self.gridmap):
+            if n.position not in self.desired_kpath:    
+                print("Objetivo")
+                print(n.position)
+                node = oposite(self.gridmap, box, n)
+                path_keeper = search(self.gridmap, keeper, node, 'keeper')
+                self.solution += path_keeper
+                if path_keeper:
+                    
+                    return True
+
+        return False
+
+    def refresh_gridmap(self, box, obj_box, keeper, obj_keeper):
         # guardar modificação ao gridmap
-        modification_keeper = (box, obj_box)
-        modification_box = (keeper, obj_keeper)
-        self.gridmap_history.append([modification_keeper, modification_box])
+        modification_keeper = (keeper, obj_keeper)
+        modification_box = (box, obj_box)
+        self.gridmap_history.insert(0, [modification_keeper, modification_box])
+        
+        self.__trade_position(box, obj_box, keeper, obj_keeper)
 
-         # modificar keeper gridmap
+    def reset_gridmap(self):
+        for move in self.gridmap_history:
+            obj_keeper, keeper,  = move[0]
+            obj_box, box  = move[1]
+            
+            self.__trade_position(box, obj_box, keeper, obj_keeper)
+
+    def __trade_position(self, box, obj_box, keeper, obj_keeper):
+        # modificar keeper gridmap
         tmp = keeper.symbol
         keeper.symbol = obj_keeper.symbol
         obj_keeper.symbol = tmp
 
         # modificar box gridmap
-        tmp = box.symbol
-        box.symbol = obj_box.symbol
-        obj_box.symbol = tmp
+        if box.symbol == '*':           
+            box.symbol = '.'
+            obj_box.symbol = '$'
+        elif box.symbol == '.':
+            box.symbol = '-'
+            obj_box.symbol = '*'
+        else:
+            tmp = box.symbol
+            box.symbol = obj_box.symbol
+            obj_box.symbol = tmp
